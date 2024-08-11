178b7049d09ec8b12fced3063085a7f165cbbeec
shubham12-prof <shubhsemwal12@gmail.com>
2024-08-12 00:07:00 +0530

    mongo target

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://mern-vercel-backend-1.onrender.com',
       changeOrigin: true,
     })
   );
